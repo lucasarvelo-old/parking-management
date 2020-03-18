87c3be148800d6c85f28722a3410bc4adb0827d1
Lucas Arvelo <lucasarvelo@gmail.com>
2020-03-17 20:15:51 -0400

    add conf file

---
+++ config.js
@@ -0,0 +1,22 @@
+// Total Number of Parking Spots
+
+const totalParkingSpots = 150;
+
+// Starting Rate per hour
+
+const startingRate = 3;
+
+// Percentage Rate Increases by Level
+
+const rateIncrease = 50;
+
+// Rate Levels
+
+const rateLevels = ['1hr', '3hr', '6hr', 'ALL DAY'];
+
+module.exports = {
+  totalParkingSpots: totalParkingSpots,
+  startingRate: startingRate,
+  rateIncrease: rateIncrease,
+  rateLevels: rateLevels,
+};
