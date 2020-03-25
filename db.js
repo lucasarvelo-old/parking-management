080b731e0dbd245a20d99115e88329f860169dc8
Lucas Arvelo <lucasarvelo@gmail.com>
2020-03-25 12:45:05 -0400

    refactor database initialization

--- db.js
+++ db.js
@@ -1,9 +1,37 @@
+const debug = require('debug')('parking-management:server');
+const mongoose = require('mongoose');
+
 // MongoDB Connection URL
-const mongoUrl = 'mongodb://localhost:27017';
+const mongoUrl = process.env.DB_URL + ':' + process.env.DB_PORT;
 
 // Database Name
-const dbName = 'parking-management';
+const dbName =
+  process.env.NODE_ENV === 'test'
+    ? 'parking-management-test'
+    : 'parking-management';
 
-module.exports = {
-  url: mongoUrl + '/' + dbName,
+// Database URL
+const url = mongoUrl + '/' + dbName;
+
+// Options
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 };
+
+const initDbConnection = async () => {
+  mongoose.connect(url, options);
+
+  const db = mongoose.connection;
+
+  db.on('error', console.error.bind(console, 'database connection error:'));
+  db.once('open', () => {
+    debug('MongoDB Client Connected!');
+  });
+
+  return db;
+};
+
+module.exports = initDbConnection;
