92e9ba0314c59f30368937d17cbad9597d570830
Lucas Arvelo <lucasarvelo@gmail.com>
2020-03-24 20:01:33 -0400

    fix parking duration over 24 hours bug

--- utils/paymentCalculator.js
+++ utils/paymentCalculator.js
@@ -1,8 +1,16 @@
 module.exports = ({ checkInDate, checkOutDate, rates }) => {
-  const parkingDuration = require('./parkingDurationCalculator')({
-    checkInDate: checkInDate,
-    checkOutDate: checkOutDate,
-  });
+  try {
+    const parkingDuration = require('./parkingDurationCalculator')({
+      checkInDate: checkInDate,
+      checkOutDate: checkOutDate,
+    });
 
-  return rates.find(rate => rate.duration >= parkingDuration).price;
+    const rate =
+      rates.find(rate => rate.duration >= parkingDuration) ||
+      rates[rates.length - 1]; //use the biggest fee when the duration exceeds all rate levels
+
+    return rate.price;
+  } catch (error) {
+    return console.log(error);
+  }
 };
