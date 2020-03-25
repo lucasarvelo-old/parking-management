6bb755754662ac985657c0a2998ea268d911aafb
Lucas Arvelo <lucasarvelo@gmail.com>
2020-03-24 20:02:05 -0400

    fix error handling

--- utils/priceListGenerator.js
+++ utils/priceListGenerator.js
@@ -4,20 +4,24 @@ const priceListGenerator = ({
   listLength,
   priceList = [basePrice.toFixed(2)],
 }) => {
-  const percentage = parseFloat(increaseRate / 100);
+  try {
+    const percentage = parseFloat(increaseRate / 100);
 
-  if (listLength >= 2) {
-    priceList.push((basePrice * percentage + basePrice).toFixed(2));
+    if (listLength >= 2) {
+      priceList.push((basePrice * percentage + basePrice).toFixed(2));
 
-    priceListGenerator({
-      increaseRate: increaseRate,
-      basePrice: basePrice * percentage + basePrice,
-      listLength: --listLength,
-      priceList: priceList,
-    });
+      priceListGenerator({
+        increaseRate: increaseRate,
+        basePrice: basePrice * percentage + basePrice,
+        listLength: --listLength,
+        priceList: priceList,
+      });
+    }
+
+    return priceList;
+  } catch (error) {
+    return console.log(error);
   }
-
-  return priceList;
 };
 
 module.exports = priceListGenerator;
